c27f7e5d7f5d25e0c055c7850303512c5e4116b6
chriswebb09 <christopher.webb@flatironschool.com>
2016-08-05 10:49:18 -0400

    Added CensusAPIClient.swift USAJobsAPIClient.swift CitySDKAPIClient.swift CensusData.swift USAJobsData.swift

---
+++ ProjectOvaltine/ProjectOvaltine/CitySDKAPIClient.swift
@@ -0,0 +1,14 @@
+//
+//  CitySDKAPIClient.swift
+//  ProjectOvaltine
+//
+//  Created by Christopher Webb-Orenstein on 8/5/16.
+//  Copyright © 2016 Christopher Webb-Orenstein. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class CitySDKAPIClient {
+    
+}
