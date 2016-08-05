5ebcf42d563e7dc411ab9409bb181f399d2ac12c
chriswebb09 <christopher.webb@flatironschool.com>
2016-08-05 13:47:22 -0400

    Added LaborStaticsData.swift to /Models

---
+++ ProjectOvaltine/ProjectOvaltine/Utilities/LaborStatisticsAPIClient.swift
@@ -0,0 +1,14 @@
+//
+//  LaborStatisticsAPIClient.swift
+//  ProjectOvaltine
+//
+//  Created by Christopher Webb-Orenstein on 8/5/16.
+//  Copyright © 2016 Christopher Webb-Orenstein. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class LaborStatisticsAPIClient {
+    
+}
