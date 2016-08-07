da83b02fceaa4972fff842a80a67e9232a6e7caa
chriswebb09 <christopher.webb@flatironschool.com>
2016-08-07 17:14:42 -0400

    CitySDKData.swift file added

---
+++ ProjectOvaltine/ProjectOvaltine/Models/CitySDKData.swift
@@ -0,0 +1,14 @@
+//
+//  CitySDKData.swift
+//  ProjectOvaltine
+//
+//  Created by Christopher Webb-Orenstein on 8/7/16.
+//  Copyright © 2016 Christopher Webb-Orenstein. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+class citySDKData {
+    
+}
