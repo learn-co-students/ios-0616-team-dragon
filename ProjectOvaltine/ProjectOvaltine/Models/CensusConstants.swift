99968b747b9abb67569538d548c50d3476f71183
chriswebb09 <christopher.webb@flatironschool.com>
2016-08-09 18:33:43 -0400

    Added census params to CensusConstants - work in progress

--- ProjectOvaltine/ProjectOvaltine/Models/CensusConstants.swift
+++ ProjectOvaltine/ProjectOvaltine/Models/CensusConstants.swift
@@ -9,7 +9,30 @@
 import UIKit
 
 struct CensusConstants {
-    static let CensusKeys = ["commute_time_public_transport":"B08136_007E", "commute_time":"B08136_001E", "commute_time_walked":"B08136_011E", "education_bachelors": "B15003_022E", "education_high_school": "B15003_017E", "education_none": "B15003_002E", ]
+    static let CENSUS_REQUEST_PARAMS = ["commute_time_public_transport":"B08136_007E",
+                                                         "commute_time":"B08136_001E",
+                                                  "commute_time_walked":"B08136_011E",
+                                                  "education_bachelors":"B15003_022E",
+                                                "education_high_school":"B15003_017E",
+                                                       "education_none":"B15003_002E",
+                                               "education_professional":"B15003_024E",
+                                      "employment_civilian_labor_force":"B23025_003E",
+                                                  "employment_employed":"B23025_004E",
+                                               "employment_labor_force":"B23025_002E",
+                                           "employment_not_labor_force":"B23025_007E",
+                                                 "employment_unemploye":"B23025_005E",
+                                                               "income":"B19013_001E",
+                                                    "income_per_capita":"B19301_001E",
+                                                 "median_contract_rent":"B25058_001E",
+                                                    "median_female_age":"B01002_003E",
+                                                    "median_gross_rent":"B25064_001E",
+                                                    "median_home_value":"B25077_001E",
+                                       "median_house_construction_year":"B25035_001E",
+                                                      "median_male_age":"B25035_001E",
+                                                           "population":"B01003_001E",
+                                                              "poverty":"B17001_002E",
+                                                       "poverty_family":"B17012_002E",
+                                               "poverty_family_married":"B17012_003E"]
 }
 
 
