a6e8ee0d1478d6a583f353139f519075845f8a72
chriswebb09 <christopher.webb@flatironschool.com>
2016-08-10 12:12:28 -0400

    Added MapView.swift and InitialViewController.swift files - work in progress:Major project restructuring

---
+++ ProjectOvaltine/ProjectOvaltine/Controllers/InitialViewController.swift
@@ -0,0 +1,13 @@
+//
+//  InitialViewController.swift
+//  ProjectOvaltine
+//
+//  Created by Christopher Webb-Orenstein on 8/10/16.
+//  Copyright © 2016 Christopher Webb-Orenstein. All rights reserved.
+//
+
+import UIKit 
+
+class InitialViewController: UIViewController {
+
+}
