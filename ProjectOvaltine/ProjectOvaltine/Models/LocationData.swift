39419e27e3bc81047963b491af3b86e2f8467c48
chriswebb09 <christopher.webb@flatironschool.com>
2016-08-11 18:07:07 -0400

    merge deconflicting

--- ProjectOvaltine/ProjectOvaltine/Models/LocationData.swift
+++ ProjectOvaltine/ProjectOvaltine/Models/LocationData.swift
@@ -14,7 +14,5 @@ struct LocationData {
     let regionRadius: CLLocationDistance = 1000
     let coordinates = [["34.4313","-118.59890"],["34.4274","-118.60246"], ["34.4268","-118.60181"], ["34.4202","-118.6004"], ["34.42013","-118.59239"], ["34.42049","-118.59051"], ["34.42305","-118.59276"], ["34.42557","-118.59289"], ["34.42739","-118.59171"]]
     var zipLocation : CLLocation! = nil
-//    let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
-//                                                              self.regionRadius * 2.0, self.regionRadius * 2.0)
 }
 
