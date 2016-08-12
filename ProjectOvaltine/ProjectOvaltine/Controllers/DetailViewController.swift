258d274ef6625358c4fcff3f08b7f16cd68773b4
chriswebb09 <christopher.webb@flatironschool.com>
2016-08-12 14:02:33 -0400

    readding content from files

---
+++ ProjectOvaltine/ProjectOvaltine/Controllers/DetailViewController.swift
@@ -0,0 +1,49 @@
+//
+//  DetailViewController.swift
+//  ProgrammaticTabBar
+//
+//  Created by John Hussain on 8/8/16.
+//  Copyright © 2016 John Hussain. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UITabBarController, UITabBarControllerDelegate {
+    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        delegate = self
+//        self.initTabBarController()
+//        self.tabBar.tintColor = UIColor.blueColor()
+//    }
+//    
+//    func initTabBarController() {
+//        let tabViewController1 = TabViewController1()
+//        let tabViewController2 = TabViewController2()
+//        let tabViewController3 = TabViewController3()
+//        let tabViewController4 = TabViewController4()
+//        let tabViewController5 = TabViewController5()
+//        
+//        let item1 = tabViewController1
+//        let item2 = tabViewController2
+//        let item3 = tabViewController3
+//        let item4 = tabViewController4
+//        let item5 = tabViewController5
+//        
+//        let icon1 = UITabBarItem(title: "Statistics", image: UIImage(named: "futures.png"), tag: 0)
+//        let icon2 = UITabBarItem(title: "Finance", image: UIImage(named: "money_bag.png"), tag: 1)
+//        let icon3 = UITabBarItem(title: "Education", image: UIImage(named: "classroom.png"), tag: 2)
+//        let icon4 = UITabBarItem(title: "Transportation", image: UIImage(named: "bus.png"), tag: 3)
+//        let icon5 = UITabBarItem(title: "Demographics", image: UIImage(named: "conference.png"), tag: 4)
+//        
+//        item1.tabBarItem = icon1
+//        item2.tabBarItem = icon2
+//        item3.tabBarItem = icon3
+//        item4.tabBarItem = icon4
+//        item5.tabBarItem = icon5
+//        
+//        let controllers = [item1, item2, item3, item4, item5]
+//        self.viewControllers = controllers
+//
+//    }
+}
--- ProjectOvaltine/ProjectOvaltine/Controllers/EconomicDataViewController.swift
+++ ProjectOvaltine/ProjectOvaltine/Controllers/EconomicDataViewController.swift
@@ -10,4 +10,52 @@ import UIKit
 
 class EconomicDataViewController: UIViewController {
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.whiteColor()
+        navBar()
+        detailImage()
+    }
+    
+    func detailImage() {
+        //********** creating UIImageView Programmatically******//
+        
+        let imageView = UIImageView(frame: CGRectMake(0, 20, 425, 690))
+        let image = UIImage(named: "detail-info.png")
+        imageView.image = image
+        //imageView.layer.cornerRadius = imageView.frame.size.width / 4
+        imageView.clipsToBounds = true
+        self.view.addSubview(imageView)
+        self.view.sendSubviewToBack(imageView)
+    }
+    
+    func navBar() {
+        let navBar: UINavigationBar = UINavigationBar(frame: CGRect(x: 0, y: 20, width: self.view.frame.width, height: 40))
+        self.view.addSubview(navBar)
+        let navItem = UINavigationItem(title: "Demographics")
+        let homeItem = UIBarButtonItem.init(title: "Home", style: .Done, target: nil, action: #selector(dismissView))
+        
+        navItem.leftBarButtonItem = homeItem
+        
+        navBar.setItems([navItem], animated: false)
+        navBar.alpha = 1.0
+        
+        let button: UIButton = UIButton(type: .Custom)
+        button.setImage(UIImage(named: "settings-4.png"), forState: UIControlState.Normal)
+        button.addTarget(self, action: #selector(settingButtonPushed), forControlEvents: UIControlEvents.TouchUpInside)
+        button.frame = CGRectMake(3, 3, 33, 33)
+        
+        let barButton = UIBarButtonItem(customView: button)
+        self.navigationItem.rightBarButtonItem = barButton
+        navItem.rightBarButtonItem = barButton
+    }
+    
+    func dismissView() {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func settingButtonPushed() {
+        //not implemented yet
+    }
 }
+
