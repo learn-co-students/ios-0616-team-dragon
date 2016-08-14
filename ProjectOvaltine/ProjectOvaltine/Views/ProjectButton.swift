48f46336af84ff73a4b8c74aa67bf31db61fa44f
chriswebb09 <christopher.webb@flatironschool.com>
2016-08-14 15:54:32 -0400

    Added ProjectButton.swift

---
+++ ProjectOvaltine/ProjectOvaltine/Views/ProjectButton.swift
@@ -0,0 +1,24 @@
+//
+//  ProjectButton.swift
+//  ProjectOvaltine
+//
+//  Created by Christopher Webb-Orenstein on 8/14/16.
+//  Copyright © 2016 Christopher Webb-Orenstein. All rights reserved.
+//
+
+import UIKit
+
+struct ProjectButton {
+    var nameButton: UIButton!
+    let screenSize: CGRect = UIScreen.mainScreen().bounds
+    init() {
+        self.nameButton = UIButton(frame: CGRectMake(20, 630, self.screenSize.width-40, 40))
+        self.nameButton.backgroundColor=UIColor.lightGrayColor()
+        self.nameButton.setTitle("PROJECT OVALTINE", forState: .Normal)
+        self.nameButton.setTitleColor(UIColor.blackColor(), forState: .Normal)
+        self.nameButton.alpha = 0.3
+        self.nameButton.layer.zPosition = 3
+        self.nameButton.layer.borderWidth = 0.3
+        self.nameButton.layer.cornerRadius = 2
+    }
+}
