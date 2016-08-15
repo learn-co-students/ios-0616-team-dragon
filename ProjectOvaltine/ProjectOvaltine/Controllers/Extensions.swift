f017318e3ffb851a66b1b3a287171afd28c462a8
chriswebb09 <christopher.webb@flatironschool.com>
2016-08-15 11:29:33 -0400

    Fixing readability Extensions.swift

--- ProjectOvaltine/ProjectOvaltine/Controllers/Extensions.swift
+++ ProjectOvaltine/ProjectOvaltine/Controllers/Extensions.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+import UIKit
+
 protocol Tabable: UITabBarControllerDelegate {
     //implemented in extension
 }
@@ -19,33 +21,34 @@ extension Tabable {
     var economicDataViewController : EconomicDataViewController { return EconomicDataViewController() }
     var educationDataViewController : EducationDataViewController { return EducationDataViewController() }
     
-    var icon1 : UITabBarItem { return UITabBarItem(title: "Statistics", image: UIImage(named: "rating.png"), tag: 0) }
-    var icon2 : UITabBarItem { return UITabBarItem(title: "Finance", image: UIImage(named: "emp.png"), tag: 1) }
-    var icon3 : UITabBarItem { return UITabBarItem(title: "Education", image: UIImage(named: "edu.png"), tag: 2) }
-    var icon4 : UITabBarItem { return UITabBarItem(title: "Transportation", image: UIImage(named: "trans.png"), tag: 3) }
-    var icon5: UITabBarItem { return UITabBarItem(title: "Demographics", image: UIImage(named: "resource.png"), tag: 4) }
+    var statisticIcon : UITabBarItem { return UITabBarItem(title: "Statistics", image: UIImage(named: "rating.png"), tag: 0) }
+    var economicIcon : UITabBarItem { return UITabBarItem(title: "Finance", image: UIImage(named: "emp.png"), tag: 1) }
+    var educationIcon : UITabBarItem { return UITabBarItem(title: "Education", image: UIImage(named: "edu.png"), tag: 2) }
+    var transportationIcon : UITabBarItem { return UITabBarItem(title: "Transportation", image: UIImage(named: "trans.png"), tag: 3) }
+    var demographicIcon: UITabBarItem { return UITabBarItem(title: "Demographics", image: UIImage(named: "resource.png"), tag: 4) }
     
     func setupTabBar() -> UITabBarController {
         let tabBarVC = UITabBarController()
         tabBarVC.delegate = self
         
-        let item1 = self.mapkitViewController
-        let item2 = self.resultViewController
-        let item3 = self.demographicDataViewController
-        let item4 = self.economicDataViewController
-        let item5 = self.educationDataViewController
+        let mapItem = self.mapkitViewController
+        let resultItem = self.resultViewController
+        let demographicItem = self.demographicDataViewController
+        let economicItem = self.economicDataViewController
+        let educationItem = self.educationDataViewController
         
-        item1.tabBarItem = icon1
-        item2.tabBarItem = icon2
-        item3.tabBarItem = icon3
-        item4.tabBarItem = icon4
+        mapItem.tabBarItem = statisticIcon
+        resultItem.tabBarItem = statisticIcon
+        demographicItem.tabBarItem = educationIcon
+        economicItem.tabBarItem = economicIcon
+        educationItem.tabBarItem = educationIcon
         
-        item5.tabBarItem = icon5
-        let controllers = [item1, item2, item3, item4, item5]
+        let controllers = [mapItem, resultItem, demographicItem, economicItem, educationItem]
         tabBarVC.viewControllers = controllers
         return tabBarVC
     }
 }
+
 
 protocol Navigable {
     //implemented in extension
