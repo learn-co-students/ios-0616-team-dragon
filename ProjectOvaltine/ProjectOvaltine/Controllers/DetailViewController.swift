65a4c27a8d039abc202da99eb8935352095749d9
chriswebb09 <christopher.webb@flatironschool.com>
2016-08-15 16:53:18 -0400

    Readability

--- ProjectOvaltine/ProjectOvaltine/Controllers/DetailViewController.swift
+++ ProjectOvaltine/ProjectOvaltine/Controllers/DetailViewController.swift
@@ -15,7 +15,7 @@ class DetailViewController: UIViewController, UITableViewDelegate, UITableViewDa
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.whiteColor()
-        navBar()
+        //navBar()
         statsTableView()
         ratingTextView()
     }
@@ -74,31 +74,7 @@ class DetailViewController: UIViewController, UITableViewDelegate, UITableViewDa
         print(detailsArray[indexPath.row])
         
     }
-    
-    
-    
-    
-    func navBar() {
-        let navBar: UINavigationBar = UINavigationBar(frame: CGRect(x: 0, y: 20, width: self.view.frame.width, height: 50))
-        self.view.addSubview(navBar)
-        let navItem = UINavigationItem(title: "Statistics")
-        let homeItem = UIBarButtonItem.init(title: "Home", style: .Done, target: nil, action: #selector(dismissView))
-        navItem.leftBarButtonItem = homeItem
-        navBar.setItems([navItem], animated: false)
-        navBar.alpha = 1.0
-        navBar.layer.zPosition = 3
-        let button: UIButton = UIButton(type: .Custom)
-        button.setImage(UIImage(named: "settings-4.png"), forState: UIControlState.Normal)
-        button.addTarget(self, action: #selector(settingButtonPushed), forControlEvents: UIControlEvents.TouchUpInside)
-        button.frame = CGRectMake(3, 3, 33, 33)
-        
-        let barButton = UIBarButtonItem(customView: button)
-        self.navigationItem.rightBarButtonItem = barButton
-        navItem.rightBarButtonItem = barButton
-        navBar.barTintColor = UIColor.whiteColor()
-        navBar.titleTextAttributes = [ NSFontAttributeName: UIFont(name:"AppleSDGothicNeo-Regular", size: 34)!,  NSForegroundColorAttributeName: UIColor.blackColor()]
-    }
-    
+
     func dismissView() {
         SwiftSpinner.showWithDuration(99.0, title: "TEAM DRAGON")
         SwiftSpinner.setTitleFont(UIFont(name: "Futura", size: 33.0))
