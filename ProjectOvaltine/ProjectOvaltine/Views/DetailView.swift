82568eabac29957a9ecad5ada814116b3a1794bf
chriswebb09 <christopher.webb@flatironschool.com>
2016-08-15 17:30:35 -0400

    Added DetailView.swift/edit for readability - work in progress

--- ProjectOvaltine/ProjectOvaltine/Views/DetailView.swift
+++ ProjectOvaltine/ProjectOvaltine/Views/DetailView.swift
@@ -1,9 +1,19 @@
 //
-//  DemographicView.swift
+//  DetailView.swift
 //  ProjectOvaltine
 //
 //  Created by Christopher Webb-Orenstein on 8/15/16.
 //  Copyright © 2016 Christopher Webb-Orenstein. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class DetailView: UIView {
+    var topicLabel: UILabel! = UILabel()
+    var scoreLabel: UILabel! = UILabel()
+    var descriptionLabel: UILabel = UILabel()
+    
+    required init?(coder: NSCoder = NSCoder.empty()) {
+        super.init(coder: coder)
+    }
+}
