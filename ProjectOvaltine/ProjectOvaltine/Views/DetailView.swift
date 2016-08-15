7ec622ba4616a193dd8ec947c73299ede0d60d03
chriswebb09 <christopher.webb@flatironschool.com>
2016-08-15 17:29:10 -0400

    Added DetailView.swift/edit for readability - work in progress

---
+++ ProjectOvaltine/ProjectOvaltine/Views/DetailView.swift
@@ -0,0 +1,9 @@
+//
+//  DemographicView.swift
+//  ProjectOvaltine
+//
+//  Created by Christopher Webb-Orenstein on 8/15/16.
+//  Copyright © 2016 Christopher Webb-Orenstein. All rights reserved.
+//
+
+import Foundation
