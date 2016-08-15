ec0c25cf1ab6bc161782311d78d88f0793c0b9d8
chriswebb09 <christopher.webb@flatironschool.com>
2016-08-15 16:48:44 -0400

    new code

--- ProjectOvaltine/ProjectOvaltine/Controllers/TabBarController.swift
+++ ProjectOvaltine/ProjectOvaltine/Controllers/TabBarController.swift
@@ -7,13 +7,13 @@
 //
 
 import UIKit
+import SwiftSpinner
 
 class TabBarController: UITabBarController, UITabBarControllerDelegate, Navigable {
     override func viewDidLoad() {
         super.viewDidLoad()
         delegate = self
-        self.view.addSubview(self.setupNavBar())
-        
+        self.setupNav()
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -37,5 +37,32 @@ class TabBarController: UITabBarController, UITabBarControllerDelegate, Navigabl
     func tabBarController(tabBarController: UITabBarController, shouldSelectViewController viewController: UIViewController) -> Bool {
         print("Should select viewController: \(viewController.title) ?")
         return true;
+    }
+    
+    func setupNav() {
+        let nav = self.setupNavBar()
+        let homeItem = UIBarButtonItem.init(title: "Home", style: .Done, target: nil, action: #selector(dismissView))
+        let navItem = UINavigationItem(title: "Results")
+        navItem.leftBarButtonItem = homeItem
+        nav.setItems([navItem], animated: false)
+        let button: UIButton = UIButton(type: .Custom)
+        button.setImage(UIImage(named: "settings-4.png"), forState: UIControlState.Normal)
+        button.addTarget(self, action: #selector(settingButtonPushed), forControlEvents: UIControlEvents.TouchUpInside)
+        button.frame = CGRectMake(3, 3, 33, 33)
+        let barButton = UIBarButtonItem(customView: button)
+        self.navigationItem.rightBarButtonItem = barButton
+        navItem.rightBarButtonItem = barButton
+        self.view.addSubview(nav)
+    }
+    
+    func dismissView() {
+        SwiftSpinner.showWithDuration(99.0, title: "TEAM DRAGON")
+        SwiftSpinner.setTitleFont(UIFont(name: "Futura", size: 33.0))
+        dismissViewControllerAnimated(true, completion: nil)
+        SwiftSpinner.hide()
+    }
+    
+    func settingButtonPushed() {
+        print("Settings Pushed")
     }
 }
