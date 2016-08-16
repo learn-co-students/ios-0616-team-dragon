dd246fdaeb420d5619169430f30437b65f6cc858
chriswebb09 <christopher.webb@flatironschool.com>
2016-08-16 09:05:00 -0400

    test

---
+++ ProjectOvaltine/ProjectOvaltine/Controllers/NavBar.swift
@@ -0,0 +1,22 @@
+//
+//  NavBar.swift
+//  ProjectOvaltine
+//
+//  Created by Christopher Webb-Orenstein on 8/16/16.
+//  Copyright © 2016 Christopher Webb-Orenstein. All rights reserved.
+//
+
+import UIKit
+
+class NavBar {
+    let width: CGFloat = UIScreen.mainScreen().bounds.width
+    
+    func setup() -> UINavigationBar {
+        let navBar: UINavigationBar = UINavigationBar(frame: CGRect(x: 0, y: 20, width:self.width, height: 50))
+        navBar.alpha = 1.0
+        navBar.layer.zPosition = 3
+        navBar.barTintColor = UIColor.whiteColor()
+        navBar.titleTextAttributes = [ NSFontAttributeName: UIFont(name:"AppleSDGothicNeo-Regular", size: 34)!,  NSForegroundColorAttributeName: UIColor.blackColor()]
+        return navBar
+    }
+}
