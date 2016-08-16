035f188a5406f8d6ceb2a3bb734b88905fc53288
colinfwalsh <colinfwalsh@gmail.com>
2016-08-16 10:12:20 -0400

    All good - all files fixed - working together

--- ProjectOvaltine/ProjectOvaltine/Controllers/MapKitViewController.swift
+++ ProjectOvaltine/ProjectOvaltine/Controllers/MapKitViewController.swift
@@ -110,13 +110,13 @@ class MapKitViewController: UIViewController, MKMapViewDelegate, UISearchControl
         self.overlayArray.removeAll()
         self.getLocationFromZipcode(self.searchController.text!)
         
-        //        let time = dispatch_time(dispatch_time_t(DISPATCH_TIME_NOW), 5 * Int64(NSEC_PER_SEC))
-        //        dispatch_after(time, dispatch_get_main_queue()) {
-        //
-        //            let detailVC = DetailViewController()
-        //            self.showViewController(detailVC, sender: nil)
-        //            self.searchController.text?.removeAll()
-        //        }
+                let time = dispatch_time(dispatch_time_t(DISPATCH_TIME_NOW), 5 * Int64(NSEC_PER_SEC))
+                dispatch_after(time, dispatch_get_main_queue()) {
+        
+                    let detailVC = StatsViewController()
+                    self.showViewController(detailVC, sender: nil)
+                    self.searchController.text?.removeAll()
+                }
     }
     
     //    func getLocationFromSearchField(userSearch: String) {
