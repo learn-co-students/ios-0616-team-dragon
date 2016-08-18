52307a621e9b50d5860c7e2b9de1f7c300f01c7f
chriswebb09 <christopher.webb@flatironschool.com>
2016-08-18 17:56:27 -0400

    Readability

--- ProjectOvaltine/ProjectOvaltine/Views/ResultView.swift
+++ ProjectOvaltine/ProjectOvaltine/Views/ResultView.swift
@@ -81,7 +81,7 @@ class ResultView: UIView {
         self.graphView.thickness = 9
         self.graphView.labelFont = UIFont.systemFontOfSize(80, weight: UIFontWeightThin)
         self.graphView.labelColor = UIColor.blueColor()
-        self.graphView.gaugeBackgroundColor = UIColor.lightGrayColor()
+        self.graphView.gaugeBackgroundColor = randomColor()
         self.addSubview(graphView)
     }
     
@@ -109,6 +109,13 @@ class ResultView: UIView {
     }
     
     func setupView() {
+    }
+    
+    private func randomColor() -> UIColor {
+        let hue = ( CGFloat(arc4random() % 256) / 256.0 )               //  0.0 to 1.0
+        let saturation = ( CGFloat(arc4random() % 128) / 256.0 ) + 0.5  //  0.5 to 1.0, away from white
+        let brightness = ( CGFloat(arc4random() % 128) / 256.0 ) + 0.5  //  0.5 to 1.0, away from black
+       return UIColor(hue: hue, saturation: saturation, brightness: brightness, alpha: 1)
     }
     
 }
