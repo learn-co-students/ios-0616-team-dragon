8e999512ada38fd5ede364cb8a6f8d4dc58df0e8
chriswebb09 <christopher.webb@flatironschool.com>
2016-08-22 17:05:13 -0400

    moving to constraints from static positioning

--- ProjectOvaltine/ProjectOvaltine/Controllers/StatsViewController.swift
+++ ProjectOvaltine/ProjectOvaltine/Controllers/StatsViewController.swift
@@ -37,7 +37,7 @@ class StatsViewController: UITableViewController {
     }
     
    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-        return 110
+        return 80
     }
     
     
