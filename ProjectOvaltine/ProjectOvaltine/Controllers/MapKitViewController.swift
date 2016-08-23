73a48b414be2d04af638ae4f036dbe91ca19c267
colinfwalsh <colinfwalsh@gmail.com>
2016-08-23 00:56:29 -0400

    Still working

--- ProjectOvaltine/ProjectOvaltine/Controllers/MapKitViewController.swift
+++ ProjectOvaltine/ProjectOvaltine/Controllers/MapKitViewController.swift
@@ -124,6 +124,7 @@ class MapKitViewController: UIViewController, MKMapViewDelegate, UISearchControl
         //I don't know how to convert this if condition to swift 1.2 but you can remove it since you don't have any other button in the annotation view
         if (control as? UIButton)?.buttonType == UIButtonType.DetailDisclosure {
             
+        
             let detailVC = TabBarController()
             self.showViewController(detailVC, sender: nil)
             self.searchController.text?.removeAll()
@@ -208,12 +209,23 @@ class MapKitViewController: UIViewController, MKMapViewDelegate, UISearchControl
                         }
                     }
                     
-                    let USScore = ScoreModel(originDataPoints: self!.USAbsoluteDictionary, comparisonDataPoints: self!.cityAbsoluteDictionary)
                     
-                    self!.store.comparisonData = USScore
+                    dispatch_async(dispatch_get_main_queue()) { [weak self] in
+                        let USScore = ScoreModel(originDataPoints: self!.USAbsoluteDictionary, comparisonDataPoints: self!.cityAbsoluteDictionary)
+                        
+                        self!.store.comparisonData = USScore
+                        
+                        self!.store.comparisonData?.getEconomicScore()
+                        self!.store.comparisonData?.getTransitScore()
+                        self!.store.comparisonData?.getEducationScore()
+                        
+                    }
+                   
                     
                     
                 })
+                
+               
                 
                 SwiftSpinner.showWithDuration(2.0, title: "Ovaltine")
                 SwiftSpinner.setTitleFont(UIFont(name: "Futura", size: 33.0))
--- ProjectOvaltine/ProjectOvaltine/Models/ScoreModel.swift
+++ ProjectOvaltine/ProjectOvaltine/Models/ScoreModel.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 struct ScoreModel {
-
+    
     var scoreName: String
     var score: Int
     var originDataPoints: [String: String]?
@@ -28,26 +28,26 @@ struct ScoreModel {
         self.transitScore = 0
         self.demographicScore = 0
         self.educationScore = 0
-    
-    
+        
+        
     }
     
     //Origin should be the higher level - US -> State -> County -> City
-        //Otherwise it should just be the starting destination
+    //Otherwise it should just be the starting destination
     mutating func getEconomicScore() -> String {
         
         
         let origin = Double(self.originDataPoints!["Median household income"]!)
         let comparison = Double(self.comparisonDataPoints!["Median household income"]!)
         
-  
         
-//        Subtracts the comparison level with the origin level 
-//            Should take the lowever level, for instance, and subtract by the higher level
-//            Ex. City Avg - US Avg which should produce a positive number
+        
+        //        Subtracts the comparison level with the origin level
+        //            Should take the lowever level, for instance, and subtract by the higher level
+        //            Ex. City Avg - US Avg which should produce a positive number
         let subtractedValueForPercentage = comparison! - origin!
         
-//        Takes the origin data and divides by the subtractedValue to get a percentage, then adds by 100
+        //        Takes the origin data and divides by the subtractedValue to get a percentage, then adds by 100
         let percentageChange = ((origin!/subtractedValueForPercentage) * 100.0)
         
         print(percentageChange)
@@ -56,53 +56,102 @@ struct ScoreModel {
         
         return String(percentageChange)
     }
-//
-//    mutating func getTransitScore() -> String {
-//        var returnValue = " "
-//        var sum = 0
-//        for data in self.dataPoints!.values {
-//            if let data = Int(data) {
-//                sum = sum + data
-//            }
-//        }
-//        self.transitScore = sum / self.dataPoints!.count
-//        returnValue = String(sum)
-//        return returnValue
-//    }
-//    
-//    mutating func getEducationScore() -> String {
-//        var returnValue = " "
-//        var sum = 0
-//        for data in self.dataPoints!.values {
-//            if let data = Int(data) {
-//                sum = sum + data
-//            }
-//            
-//        }
-//        self.educationScore = sum / (self.dataPoints!.count * 10)
-//        returnValue = String(sum)
-//        return returnValue
-//    }
-//    
-//    mutating func getDemographicScore() -> String {
-//        var returnValue = " "
-//        var sum = 0
-//        for data in self.dataPoints!.values {
-//            if let data = Int(data) {
-//                sum = sum + data
-//            }
-//            
-//        }
-//        self.demographicScore = sum / self.dataPoints!.count
-//        returnValue = String(sum)
-//        return returnValue
-//    }
+    
+    mutating func getTransitScore() -> String {
+        
+        let origin = Double(self.originDataPoints!["Average travel time to work one way in minutes"]!)
+        let comparison = Double(self.comparisonDataPoints!["Average travel time to work one way in minutes"]!)
+        
+        
+        print(origin)
+        print(comparison)
+        //        Subtracts the comparison level with the origin level
+        //            Should take the lowever level, for instance, and subtract by the higher level
+        //            Ex. City Avg - US Avg which should produce a positive number
+        //let subtractedValueForPercentage = comparison? - origin?
+        
+        //        Takes the origin data and divides by the subtractedValue to get a percentage, then adds by 100
+        // let percentageChange = ((origin!/subtractedValueForPercentage) * 100.0)
+        
+        //    print(percentageChange)
+        
+        
+        
+        //return String(percentageChange)
+        
+        return ""
+    }
+    //
+    mutating func getEducationScore() -> String {
+        
+//        "002E": "No schooling completed",
+//        "017E": "High school diploma",
+//        "018E": "GED or alternative credential",
+//        "024E": "Professional school degree",
+//        "022E": "Bachelor's degree",
+//        "023E": "Master's degree",
+//        "025E": "Doctorate degree",
+        
+        //Origin data points
+        let originNoSchool = Int(self.originDataPoints!["No schooling completed"]!)
+        let originHighSchool = Int(self.originDataPoints!["High school diploma"]!)
+        let originGED = Int(self.originDataPoints!["GED or alternative credential"]!)
+        let originProfessionalDegree = Int(self.originDataPoints!["Professional school degree"]!)
+        let originBachelors = Int(self.originDataPoints!["Bachelor's degree"]!)
+        let originMasters = Int(self.originDataPoints!["Master's degree"]!)
+        let originDoctorates = Int(self.originDataPoints!["Doctorate degree"]!)
+        
+        //Comparisson data points
+        let comparisonNoSchool = Int(self.comparisonDataPoints!["No schooling completed"]!)
+        let comparisonHighSchool = Int(self.comparisonDataPoints!["High school diploma"]!)
+        let comparisonGED = Int(self.comparisonDataPoints!["GED or alternative credential"]!)
+        let comparisonProfessionalDegree = Int(self.comparisonDataPoints!["Professional school degree"]!)
+        let comparisonBachelors = Int(self.comparisonDataPoints!["Bachelor's degree"]!)
+        let comparisonMasters = Int(self.comparisonDataPoints!["Master's degree"]!)
+        let comparisonDoctorates = Int(self.comparisonDataPoints!["Doctorate degree"]!)
+        
+//        [ x1, x2, ... , xn].map(f) -> [f(x1), f(x2), ... , f(xn)]
+        
+        let originValues = [originNoSchool, originHighSchool, originGED, originProfessionalDegree, originBachelors, originMasters, originDoctorates]
+        
+        let totalOriginValues = originValues.map{$0! + $0!}
+        
+        print(totalOriginValues)
+        
+        let comparisonValues = [comparisonNoSchool, comparisonHighSchool, comparisonGED, comparisonProfessionalDegree, comparisonBachelors, comparisonMasters, comparisonDoctorates]
+        
+        let totalComparisonValues = comparisonValues.map{$0! + $0!}
+        
+        print(totalComparisonValues)
+        
+        //        Takes the origin data and divides by the subtractedValue to get a percentage, then adds by 100
+        //let percentageChange = ((origin!/subtractedValueForPercentage) * 100.0)
+        
+//        print(percentageChange)
+//        
+//        return String(percentageChange)
+        return ""
+    }
+    //
+    //    mutating func getDemographicScore() -> String {
+    //        var returnValue = " "
+    //        var sum = 0
+    //        for data in self.dataPoints!.values {
+    //            if let data = Int(data) {
+    //                sum = sum + data
+    //            }
+    //
+    //        }
+    //        self.demographicScore = sum / self.dataPoints!.count
+    //        returnValue = String(sum)
+    //        return returnValue
+    //    }
     
     mutating func getScoresDictionary() -> [String: String] {
-       // self.getEducationScore()
-       // self.getTransitScore()
+        // self.getEducationScore()
+        // self.getTransitScore()
         self.getEconomicScore()
-       // self.getDemographicScore()
+        // self.getDemographicScore()
         let returnDict = ["Education": String(self.educationScore), "Transit": String(self.transitScore), "Economic": String(self.economicScore), "Demographic": String(self.demographicScore)]
         return returnDict
     }
