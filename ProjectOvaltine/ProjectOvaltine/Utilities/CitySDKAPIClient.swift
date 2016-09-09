89aeade8fb8040d3b0aa356d656e3d9587485087
chriswebb09 <christopher.webb@flatironschool.com>
2016-09-09 17:46:32 -0400

    refactor

--- ProjectOvaltine/ProjectOvaltine/Utilities/CitySDKAPIClient.swift
+++ ProjectOvaltine/ProjectOvaltine/Utilities/CitySDKAPIClient.swift
@@ -18,6 +18,7 @@ class CitySDKAPIClient {
     let headers: Dictionary<String, String> = ["Not":"Implemented"]
     let parameters:Dictionary<String, String> = ["parameterOne": "not implemented"]
     let key = Constants.CITYSDK_API_KEY
+    
     var couldNotReturn = false
     
     // MARK: Request
@@ -29,45 +30,44 @@ class CitySDKAPIClient {
         }
         
         let request = NSMutableURLRequest(URL:url)
+        
         request.HTTPMethod = "POST"
-        request.setValue("application/json",
-                         forHTTPHeaderField: "Content-Type")
-        request.setValue(self.key,
-                         forHTTPHeaderField: "Authorization")
-        request.HTTPBody = try! NSJSONSerialization.dataWithJSONObject(params,
-                                                                       options: [])
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.setValue(self.key, forHTTPHeaderField: "Authorization")
+        request.HTTPBody = try! NSJSONSerialization.dataWithJSONObject(params, options: [])
         
         Alamofire.request(request).responseJSON { (response) in
+            
             switch response.result {
                 
             case .Success(let responseObject):
                 var cityDataPoints: [CitySDKData] = []
+                
                 let response = responseObject as! NSDictionary
                 
-                if let feat = response["features"] as? NSArray {
-                    let jsonProperties = JSON(feat[0]["properties"] as! NSDictionary)
+                guard let feat = response["features"] as? NSArray else { return }
+                guard let geo = feat[0]["geometry"] as? NSDictionary else { return }
+                
+                let jsonProperties = JSON(feat[0]["properties"] as! NSDictionary)
+                
+                if let coords = geo["coordinates"] as? NSArray {
                     
-                    if let geo = feat[0]["geometry"] as? NSDictionary {
+                    if coords[0].count > 100 {
                         
-                        if let coords = geo["coordinates"] as? NSArray {
-                            //This could probably be implemented better, but this fixes the bug where the app would crash if you were to query New York or California since they were nested differently.  It only checks for two cases, but I think it's the extent of the bug - if you find one let me know
-                            if coords[0].count > 100 {
-                                let newData = CitySDKData(json: jsonProperties, geoJSON: coords[0] as! NSArray)
-                                cityDataPoints.append(newData)
-                                
-                            } else if coords[0][0].count > 100 {
-                                
-                                if let deeperCoords = (coords[0] as! NSArray)[0] as? NSArray {
-                                    let otherData = CitySDKData(json: jsonProperties, geoJSON: deeperCoords)
-                                    cityDataPoints.append(otherData)
-                                } else {
-                                    print("ERROR - SOMETHING IS WRONG WITH THE CALL TO THE ARRAYS!")}
-                            }
-                            
-                        }
-                    }
-                    completion(cityDataPoints)
+                        let newData = CitySDKData(json: jsonProperties, geoJSON: coords[0] as! NSArray)
+                        cityDataPoints.append(newData)
+                        
+                    } else if coords[0][0].count > 100 {
+                        
+                        guard let deeperCoords = (coords[0] as! NSArray)[0] as? NSArray else { return }
+                        
+                        let otherData = CitySDKData(json: jsonProperties, geoJSON: deeperCoords)
+                        cityDataPoints.append(otherData)
+                        
+                    } else {
+                        print("ERROR - SOMETHING IS WRONG WITH THE CALL TO THE ARRAYS!")}
                 }
+                completion(cityDataPoints)
             default:
                 print("ERROR - CITYSDK CANNOT BE REACHED")
                 self.couldNotReturn = true
